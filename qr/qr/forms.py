99c2ff4c131b03f8de071fe12bd0e5651f65796c
LearnerAnisha <anishagautam012@gmail.com>
2025-08-24 14:31:32 +0545

    improved the form design

--- qr/qr/forms.py
+++ qr/qr/forms.py
@@ -1,5 +1,17 @@
 from django import forms
 
 class QRCodeForm(forms.Form):
-  restaurant_name = forms.CharField(max_length=10, label='Restaurant Name')
-  url = forms.URLField(max_length = 200, label='Menu URL')
+  restaurant_name = forms.CharField(
+    max_length=10, 
+    label='Restaurant Name',
+    widget = forms.TextInput(attrs={
+      'class': 'form-control',
+      'placeholder': 'Enter restaurant name'})
+  )
+  url = forms.URLField(
+    max_length = 200,
+    label='Menu URL',
+    widget = forms.URLInput(attrs={
+      'class': 'form-control',
+      'placeholder': 'Enter menu URL'})
+    )
